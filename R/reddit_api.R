5aa4362f9ce0b9bac1905e085ae96594e1a1436a
lisette-18 <horne146@umn.edu>
2024-03-24 15:39:27 -0500

    Data Import and Cleaning

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -2,3 +2,11 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(RedditExtractoR)
+
+#Data Import and Cleaning
+urls <- find_thread_urls(subreddit = "rstats", period = "month")
+thread_content <- get_thread_content(urls$url)
+
+rstats_tbl <- tibble(post = thread_content$threads$title,
+                        upvotes = thread_content$threads$upvotes,
+                        comments = thread_content$threads$comments)
