22d774d832b3ca42b2f7a2509d0f95eb6c8d289f
lisette-18 <horne146@umn.edu>
2024-03-24 15:51:07 -0500

    Visualization

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -10,3 +10,9 @@ thread_content <- get_thread_content(urls$url)
 rstats_tbl <- tibble(post = thread_content$threads$title,
                         upvotes = thread_content$threads$upvotes,
                         comments = thread_content$threads$comments)
+
+#Visualization
+ggplot(rstats_tbl, aes(x = upvotes, y = comments)) +
+  geom_point() +
+  geom_smooth(method = "lm", color = "pink") +
+  labs(x = "Upvotes", y = "Comments", title = "Scatterplot of the Upvotes and Comments")
